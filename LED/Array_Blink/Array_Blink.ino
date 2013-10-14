b7fa6edfa4edea431e352fcc6d6e73e797cf8b1e
andycondon <condon.andy@gmail.com>
2013-10-14 07:25:21 -0600

    No longer duplicating call to led(), which fixes #1.

--- LED/Array_Blink/Array_Blink.ino
+++ LED/Array_Blink/Array_Blink.ino
@@ -1,4 +1,4 @@
-const int BLINK_MILLIS = 50;
+const unsigned long BLINK_MILLIS = 100;
 const int NUM_LEDS = 6;
 const int LEDS[NUM_LEDS] = {13, 12, 11, 10, 9, 8};
 
@@ -9,9 +9,10 @@ int led(int ledNumber)
 
 void blink(int ledNumber, int millisOn)
 {
-  digitalWrite(led(ledNumber), HIGH);
+  int pin = led(ledNumber);
+  digitalWrite(pin, HIGH);
   delay(millisOn);
-  digitalWrite(led(ledNumber), LOW);
+  digitalWrite(pin, LOW);
 }
 
 void setup(void) 
@@ -26,11 +27,11 @@ void loop(void)
 {
   for(int i = 0; i < NUM_LEDS; i++)
   {
-    blink(led(i), BLINK_MILLIS);
+    blink(i, BLINK_MILLIS);
     delay(BLINK_MILLIS);
-    blink(led(i), BLINK_MILLIS);
+    blink(i, BLINK_MILLIS);
     delay(BLINK_MILLIS);
-    blink(led(i), BLINK_MILLIS);
+    blink(i, BLINK_MILLIS);
     
     delay(1000);
   }
