335d2c4c18048f8a58cb7471718f4b0290d95346
andycondon <condon.andy@gmail.com>
2013-10-15 21:01:00 -0600

    Made it dance a little more.

--- LED/Array_Blink/Array_Blink.ino
+++ LED/Array_Blink/Array_Blink.ino
@@ -1,18 +1,22 @@
-const unsigned long BLINK_MILLIS = 100;
+const unsigned long BLINK_MILLIS = 50;
 const int NUM_LEDS = 6;
-const int LEDS[NUM_LEDS] = {13, 12, 11, 10, 9, 8};
+const int LEDS[NUM_LEDS] = {13, 12, 8, 7, 4, 2};
+//                          0   1   2  3  4  5
 
 int led(int ledNumber)
 {
   return LEDS[ledNumber];
 }
 
-void blink(int ledNumber, int millisOn)
+void blink(int ledNumber, int millisOn, int times)
 {
-  int pin = led(ledNumber);
-  digitalWrite(pin, HIGH);
-  delay(millisOn);
-  digitalWrite(pin, LOW);
+  for(int i = 0; i < times; i++)
+  {
+    int pin = led(ledNumber);
+    digitalWrite(pin, HIGH);
+    delay(millisOn);
+    digitalWrite(pin, LOW);
+  }
 }
 
 void setup(void) 
@@ -27,12 +31,13 @@ void loop(void)
 {
   for(int i = 0; i < NUM_LEDS; i++)
   {
-    blink(i, BLINK_MILLIS);
+    blink(i, BLINK_MILLIS, 2);
     delay(BLINK_MILLIS);
-    blink(i, BLINK_MILLIS);
+  }
+  
+  for(int i = NUM_LEDS - 1; i >= 0; i--)
+  {
+    blink(i, BLINK_MILLIS, 2);
     delay(BLINK_MILLIS);
-    blink(i, BLINK_MILLIS);
-    
-    delay(1000);
   }
 }
