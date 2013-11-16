02cc66527bf4a69aff1b06e74727cadcddd6d11a
andycondon <condon.andy@gmail.com>
2013-11-16 14:46:56 -0700

    Playing around with keeping the WiFi integration small. So far its footprint is only 1.7kB.

---
+++ WiFi/WiFi.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_CC3000.h>
+#include <ccspi.h>
+#include <SPI.h>
+
+#define CC3000_TINY_DRIVER
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIV2);
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
